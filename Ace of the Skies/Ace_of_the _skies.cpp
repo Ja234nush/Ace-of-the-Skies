d3d0964f4c8d863be452e2b7e146769ed7d0e989
Ja234nush <nowakowskijan003@gmail.com>
2023-06-05 18:54:39 +0200

    Dodaj pliki projektów.

---
+++ Ace of the Skies/Ace_of_the _skies.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include"Game.h"
+
+int main()
+{   //tworzenie silnika gry
+    Game game;
+    //tworzenie petli glownej
+    while (game.running())
+    {
+        //aktualizacja gry
+        game.update();
+        //renderowanie
+        game.render();
+    }
+
+
+
+    return 0;
+}
